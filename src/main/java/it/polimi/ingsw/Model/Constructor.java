84dab74b5a85da7f18423d6532a76d1bce5b8de8
EdoStoppa <edoardo.stoppa@mail.polimi.it>
2020-03-18 21:00:28 +0100

    Implemented classes Constructor and Position. Not yet tested

--- src/main/java/it/polimi/ingsw/Model/Constructor.java
+++ src/main/java/it/polimi/ingsw/Model/Constructor.java
@@ -1,4 +1,46 @@
 package it.polimi.ingsw.Model;
 
 public class Constructor {
+
+    private boolean canMove;
+    private final int playerNumber;
+    private Position posConstructor;
+    private Position prevPosConstructor;
+
+    public Constructor(int playerNumber){
+
+        this.canMove = true;
+        this.playerNumber = playerNumber;
+    }
+
+
+    public boolean getCanMove() {
+        return canMove;
+    }
+
+    public void setCanMove(boolean canMove) {
+        this.canMove = canMove;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public Position getPos(){
+        return this.posConstructor;
+    }
+
+    public void setPos(int x, int y){
+        posConstructor.setX(x);
+        posConstructor.setY(y);
+    }
+
+    public Position getPrevPos(){
+        return this.posConstructor;
+    }
+
+    public void setPrevPos(Position old){
+        posConstructor.setX(old.getX());
+        posConstructor.setY(old.getY());
+    }
 }
