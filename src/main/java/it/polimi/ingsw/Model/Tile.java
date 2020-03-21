bad0dfcfa8ef8c75eba72cf1dc8dfa3f97db2ef2
GiamboVuolo <62034523+GiamboVuolo@users.noreply.github.com>
2020-03-21 17:11:23 +0100

    Codice Tile

--- src/main/java/it/polimi/ingsw/Model/Tile.java
+++ src/main/java/it/polimi/ingsw/Model/Tile.java
@@ -1,4 +1,53 @@
 package it.polimi.ingsw.Model;
 
 public class Tile {
+
+    private final Position position;
+    private boolean occupied;
+    private int constructionLevel;
+    private boolean dome;
+    private Constructor actualConstuctor;
+
+    public Tile(Position pos)   {
+        this.position = pos;
+        this.occupied = false;
+        this.constructionLevel = 0;
+        this.dome = false;
+        this.actualConstuctor = null;
+    }
+
+    public Position getPos()    {
+        return this.position;
+    }
+
+    public void setOccupied(boolean b)    {
+        this.occupied = b;
+    }
+
+    public boolean getOccupied()    {
+        return this.occupied;
+    }
+
+    public void setConstructionLevel() {
+        if(getConstructionLevel() < 3)  {
+            this.constructionLevel = this.constructionLevel + 1;
+        }
+        else if(getConstructionLevel() == 3)   {
+            setDome(true);
+        }
+    }
+
+    public int getConstructionLevel()   {
+        return this.constructionLevel;
+    }
+
+    public void setDome(boolean b)  {
+        this.dome = b;
+    }
+
+    public boolean getDome()    {
+        return this.dome;
+    }
 }
+
+
